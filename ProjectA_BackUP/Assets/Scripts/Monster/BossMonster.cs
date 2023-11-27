0aa84e3217fe16938c8fe3a7d57a9c0db08bb08d
HONGJEONGWOOK <cxzzxc79@gmail.com>
2023-11-27 14:52:28 +0900

    보스 공격 모션
    현재 플레이어의 모습을 공격 당했을 시 액티브 false로 만들어 놓음

--- ProjectA_BackUP/Assets/Scripts/Monster/BossMonster.cs
+++ ProjectA_BackUP/Assets/Scripts/Monster/BossMonster.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class BossMonster : MonoBehaviour
 {
+    public float frontFootAttackTime;
+    public float breathAttackTime;
+    float distance;
+
     public Animator ani { get; set; }
     float speed;
     public static BossMonster instance;
+    private CapsuleCollider BasicCapsuleCollider;
+    private BoxCollider frontFootCollider;
 
     private void Awake()
     {
         instance = this;
         ani = GetComponent<Animator>();
+        BasicCapsuleCollider = GameObject.Find("AttackPos").GetComponent<CapsuleCollider>();
+        frontFootCollider = GameObject.Find("HandAttack").GetComponent<BoxCollider>();
     }
     void Start()
     {
@@ -27,22 +35,54 @@ public class BossMonster : MonoBehaviour
 
     public void MoveBossMonster()
     {
-        if(ani.GetBool("aniBool") == true)
+        if (ani.GetBool("aniBool") == true)
         {
-            float distance = Vector3.Distance(Village.instance.player.transform.position, transform.position);
+            frontFootAttackTime += Time.deltaTime;
+            float frontTime = frontFootAttackTime;
+            breathAttackTime += Time.deltaTime;
+            float breathTime = breathAttackTime;
+            distance = Vector3.Distance(Village.instance.player.transform.position, transform.position);
             if (distance <= 8.0f)
             {
                 ani.SetInteger("aniIndex", 1);
                 speed = 0.0f;
                 transform.LookAt(Village.instance.player.transform.position);
             }
-            else if (distance >= 8.0f)
+            else if (frontFootAttackTime >= 10.0f)
+            {
+                ani.SetInteger("aniIndex", 2);
+                speed = 0.0f;
+                transform.LookAt(Village.instance.player.transform.position);
+                frontFootAttackTime -= frontTime;
+            }
+            else if (breathAttackTime >= 23.0f && distance >= 8.0f)
+            {
+                ani.SetInteger("aniIndex", 3);
+                speed = 0.0f;
+                transform.LookAt(Village.instance.player.transform.position);
+                breathAttackTime -= breathTime;
+            }
+            else
             {
                 ani.SetInteger("aniIndex", 0);
                 speed = 3.0f;
                 transform.localPosition = Vector3.MoveTowards(transform.localPosition, Village.instance.player.transform.position, Time.deltaTime * speed);
                 transform.LookAt(Village.instance.player.transform.position);
             }
+        }
+    }
+    public void BasicAttack()
+    {
+        if (Village.instance.player.capsuleCollider.bounds.Intersects(BasicCapsuleCollider.bounds))
+        {
+            Village.instance.player.gameObject.SetActive(false);
+        }
+    }
+    public void FrontFootAttack()
+    {
+        if (Village.instance.player.capsuleCollider.bounds.Intersects(frontFootCollider.bounds))
+        {
+            Village.instance.player.gameObject.SetActive(false);
         }
     }
 
